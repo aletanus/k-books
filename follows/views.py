2823dd3fa6a6e4f5be4a4312a2b9b1f1bdf02619
eskokado <eskokado@gmail.com>
2023-05-05 11:12:22 -0300

    feat: add permission student and get, create, update and delete own

--- follows/views.py
+++ follows/views.py
@@ -2,13 +2,25 @@ from rest_framework import generics
 
 from follows.models import Follow
 from follows.serializers import FollowSerializer
+from users.permissions import IsAccountStudent
 
 
 class FollowView(generics.ListCreateAPIView):
     queryset = Follow.objects.all()
     serializer_class = FollowSerializer
+    permission_classes = [IsAccountStudent]
+
+    def get_queryset(self):
+        return Follow.objects.filter(user=self.request.user)
+
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
 
 
 class FollowDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Follow.objects.all()
     serializer_class = FollowSerializer
+    permission_classes = [IsAccountStudent]
+
+    def get_queryset(self):
+        return Follow.objects.filter(user=self.request.user)
