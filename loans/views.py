1210685fc3446044049bd9b614eaa31445464aa7
eskokado <eskokado@gmail.com>
2023-05-05 12:00:49 -0300

    feat: add loans by user view with permissions IsStudentOrCollaboratorViewingStudents

--- loans/views.py
+++ loans/views.py
@@ -1,4 +1,5 @@
 from rest_framework import generics
+from rest_framework.generics import ListAPIView
 
 from loans.models import Loan
 from loans.serializers import LoanSerializer
@@ -26,3 +27,15 @@ class LoanView(generics.ListCreateAPIView):
 class LoanDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Loan.objects.all()
     serializer_class = LoanSerializer
+    permission_classes = [IsStudentOrCollaboratorViewingStudents]
+
+
+class LoansByUserView(ListAPIView):
+    serializer_class = LoanSerializer
+    permission_classes = [IsStudentOrCollaboratorViewingStudents]
+
+    def get_queryset(self):
+        user_id = self.kwargs["user_id"]
+        if self.request.user.is_superuser or (self.request.user.student and user_id == self.request.user.id):
+            return Loan.objects.filter(user_id=user_id)
+        return Loan.objects.none()
