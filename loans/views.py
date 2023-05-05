b966f56238c13178c615abc8d5e4b0336b5027a8
eskokado <eskokado@gmail.com>
2023-05-05 09:37:36 -0300

    feat: get Loan with authorization type users

--- loans/views.py
+++ loans/views.py
@@ -2,14 +2,26 @@ from rest_framework import generics
 
 from loans.models import Loan
 from loans.serializers import LoanSerializer
+from users.permissions import IsStudentOrCollaboratorViewingStudents
 
 
 class LoanView(generics.ListCreateAPIView):
     queryset = Loan.objects.all()
     serializer_class = LoanSerializer
+    permission_classes = [IsStudentOrCollaboratorViewingStudents]
+
+    def get_queryset(self):
+        if self.request.user.student:
+            return Loan.objects.filter(user=self.request.user)
+        elif self.request.user.is_superuser:
+            return Loan.objects.all()
+        else:
+            return Loan.objects.none()
+
     def perform_create(self, serializer):
         
         return super().perform_create(serializer)
+
 
 class LoanDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Loan.objects.all()
