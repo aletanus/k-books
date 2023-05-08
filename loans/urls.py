67f0d4de4ffaaca0667b4e293852e48bbe9eb119
aletanus <alessandro@altadesign.com.br>
2023-05-08 12:56:11 -0300

    feat: loan return completed.

--- loans/urls.py
+++ loans/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from copies.views import CopyListCreateView, CopyRetrieveUpdateDestroyAPIView
-from loans.views import LoanView, LoanDetailView, LoansByUserView
+from loans.views import LoanView, LoanDetailView, LoansByUserView, LoanReturnView
 
 urlpatterns = [
     path("loans/", LoanView.as_view(),),
     path("loans/<int:pk>", LoanDetailView.as_view(),),
     path("loans/user/<int:user_id>/", LoansByUserView.as_view()),
+    path('loans/<int:pk>/update-date-return/', LoanReturnView.as_view()),
 ]
